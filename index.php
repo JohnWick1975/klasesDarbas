427736bea40ed31e5500e753efe894552117727b
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-05 12:01:36 +0200

    array => array kas 2 elementa be klaidos

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
 
-$a = array(10, 20, 30, 50, 60, 70, 80);
+$a = array(10, 20, 30, 50, 60, 70, 80, 90);
 $count = count($a);
 $b = [];
-for ($x = 0; $x <= $count; $x +=2){
+
+for ($x = 0; $x < $count; $x += 2) {
     $b[] = $a[$x];
 }
-
-foreach ($b as $array){
+foreach ($b as $array) {
     print $array . '<br>';
 }
+
+$c = 10;
+$d = $c++;
+
+print "c = $c <br> d = $d";
