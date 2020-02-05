2f4a6d9747a879c6d2d87c66ffc9a34f0a4d3bb4
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-05 10:40:34 +0200

    array is_int

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
-$vardas ='vitalijus';
-$pavarde = 'rusakas';
-$alga = 7000;
+$a = array(10, 20, 30, 1, 1, 50, 0, 0, 'jonas', 'petras');
 
-if (isset($vardas) && isset($pavarde) && $alga > 5000){
-    print 'visi duomenys yra';
-}else{
-    print "netinka";
+foreach ($a as $array) {
+    if (is_int($array)) {
+        $array *= 10;
+        print $array . '<br>';
+    } else {
+        print $array . '<br>';
+    }
 }
