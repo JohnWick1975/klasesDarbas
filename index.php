ba81349b150aaf8e2b72fe83fec6a89ddfd41a34
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-05 09:57:46 +0200

    text to array EXPLODE

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
 
-$salis = "de";
+$a = '10 maisu';
+$b = '1 maisu';
+$a = explode(' ',$a);
+$b = explode(' ',$b);
+$c = $a[0] + $b[0]  . ' ' . $a[1];
+print $c . '<br>';
 
-switch ($salis) {
-
-    case 'ca':
-        print "Kanada";
-        break;
-    case 'de':
-        print 'Vokietija';
-        break;
-    case 'uk':
-        print 'Didzioji Britanija';
-        break;
-}
+$text = 'namas medis mama';
+$text_array = explode(' ', $text);
+print_r($text_array);
