7dcaca48715e3b0e8c36dcc3722494a9f7e65e31
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-07 10:37:24 +0200

    array is failo ir i lentele

--- app/php/php.php
+++ app/php/php.php
@@ -7,7 +7,16 @@ $fo = fopen($file, 'a') or die("can't open file");
 
 $stringData = "Floppy Jalopy\n";
 
-fwrite($fo, implode(", ", $_POST) . "\n");
+fwrite($fo, implode(", ", $_POST) . ", " . "\n");
 
 fclose($fo);
+
+$content = file($file);
+$new_array = [];
+foreach ($content as $value) {
+    $array = explode(", ", $value);
+    array_pop($array);
+    array_push($new_array, $array);
+}
+var_dump($new_array);
 ?>
--- index.php
+++ index.php
@@ -15,11 +15,30 @@
 		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 	<body>
-		<form method="post">
-			<input type="text" name="name">
-			<input type="email" name="email">
-			<button type="submit">Submit</button>
-		</form>
+		<div class="container">
+			<form method="post">
+				<lable >Vardas</lable>
+				<input class="d-block" type="text" name="name">
+				<label>Email</label>
+				<input class="d-block mb-3" type="email" name="email">
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+		<div class="container">
+			<table class="m-auto">
+				<tr class="text-center">
+					<th>Vardas</th>
+					<th>E-mailas</th>
+				</tr>
+			<?php foreach ($new_array as $value): ?>
+				<tr class="border border-dark bg-light p-3">
+					<?php foreach ($value as $cont): ?>
+					<td class="border border-dark bg-light p-3 w-25 text-center"><?php print $cont ?></td>
+					<?php endforeach; ?>
+				</tr>
+				<?php endforeach; ?>
+			</table>
+		</div>
 
 		<script type="text/javascript" src="assets/js/jquery.js"></script>
 		<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
