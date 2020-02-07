a8ecd48de9cf2dd59f145c9edc01989bb1bcd38e
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-07 14:39:35 +0200

    isgerta buteliu programos FINISH

--- app/php/php.php
+++ app/php/php.php
@@ -1,6 +1,6 @@
 <?php
 
-$dezesDydis = 24;
+$dezesDydis = 20;
 $savaite = 0;
 $menuo = 0;
 $kiekIsgeriau = 0;
@@ -9,16 +9,19 @@ $kiekIsgeriau = 0;
 for ($s = 1; $s <= 4; $s++) {
     $penktadienis = rand(4, 6);
     $sestadienis = rand(1, 2);
+    $kiekIsgeriau = 0;
     for ($x = 1; $x <= 4; $x++) {
         $pirdienisKetvirtadienis = rand(1, 3);
         $kiekIsgeriau += $pirdienisKetvirtadienis;
     }
     $savaite = $kiekIsgeriau + $penktadienis + $sestadienis;
     print $savaite . ' isgerta ' . $s . 'savaite' . '<br>';
-    $kiekIsgeriau = 0;
     $menuo += $savaite;
 }
-print $menuo . ' per menesi';
+print $menuo . ' per menesi <br>';
 
+$deziuKiekis = floor($menuo / $dezesDydis);
+$buteliuLikutis = $menuo % $dezesDydis;
 
-
+print 'deziu kiekis ' . $deziuKiekis;
+print '<br>buteliu likutis ' . $buteliuLikutis;
--- index.php
+++ index.php
@@ -15,8 +15,20 @@
 		<link rel="stylesheet" href="assets/css/style.css">
 	</head>
 	<body>
-
-
+		<div class="container">
+			<div>
+				<?php for ($x = 1; $x <= $deziuKiekis; $x++) : ?>
+					<img src="https://i.pinimg.com/474x/94/43/ab/9443ab87302b51ad59d70656ff048c5c--jack-daniels-daniel-oconnell.jpg"
+						 alt="">
+                <?php endfor; ?>
+			</div>
+			<div>
+				<?php for ($x = 1; $x <= $buteliuLikutis; $x++) : ?>
+					<img src="http://oldwiskyclub.ru/img/p8bbc877689c9a60f4b1d9605853fae52-w100-h200-ti/viski-dzhek-deniels-tennessi-master-distiller.jpg"
+						 alt="bottle">
+                <?php endfor; ?>
+			</div>
+		</div>
 		<script type="text/javascript" src="assets/js/jquery.js"></script>
 		<script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
 		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
