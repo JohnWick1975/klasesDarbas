080072199f60eaa46beea9fb6b94f08a43d2d549
JohnWick1975 <vitalijus.rusakas@gmail.com>
2020-02-10 09:56:14 +0200

    funkcija metai

--- app/php/php.php
+++ app/php/php.php
@@ -1,29 +1,11 @@
 <?php
 
-$dezesDydis = 20;
-$savaite = 0;
-$menuo = 0;
-$kiekIsgeriau = 0;
-
-
-for ($s = 1; $s <= 4; $s++) {
-    $penktadienis = rand(4, 6);
-    $sestadienis = rand(1, 2);
-    $kiekIsgeriau = 0;
-    for ($x = 1; $x <= 4; $x++) {
-        $pirdienisKetvirtadienis = rand(1, 3);
-        print $pirdienisKetvirtadienis . ' buteliu isgerta ' . $x . ' diena <br>';
-        $kiekIsgeriau += $pirdienisKetvirtadienis;
+function metai($data, $data2)
+{
+    print"<select class=''>";
+    for ($i = $data; $i <= $data2; $i++) {
+        print "<option value='$data'>" . $data . "</option>";
+        $data++;
     }
-    print $kiekIsgeriau . ' nuo pirm. iki ketv.<br>';
-    $savaite = $kiekIsgeriau + $penktadienis + $sestadienis;
-    print $savaite . ' isgerta ' . $s . 'savaite' . '<br>';
-    $menuo += $savaite;
+    print "</select>";
 }
-print $menuo . ' per menesi <br>';
-
-$deziuKiekis = floor($menuo / $dezesDydis);
-$buteliuLikutis = $menuo % $dezesDydis;
-
-print 'deziu kiekis ' . $deziuKiekis;
-print '<br>buteliu likutis ' . $buteliuLikutis;
